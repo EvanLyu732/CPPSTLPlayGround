ef0e0bad858caee2337d416f56b5ac223a8ce89a
EvanLyu732 <zhengshushu1997@gmail.com>
2021-10-02 21:35:32 +0800

    add

---
+++ stl_generic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    cout << "Demonstrating generic algorithm" << endl;
+    char s[]="C++ is a better C";
+    int len=strlen(s);
+
+    const char* where = find(&s[0], &s[len], 'e');
+    assert(*where == 'e' && *(where+1) == 't');
+    cout<< " --- Ok. " <<endl;
+    return 0;
+}
---
+++ stl_map.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    map<string, long> directory;
+    directory["Bogart"] = 1234567;
+    directory["Bacall"] = 9876543;
+    directory["Cagney"] = 3459876;
+
+    string name;
+    while (cin>>name)
+        if (directory.find(name) != directory.end())
+        cout<<"The phone number for " << name 
+            << " is " << directory[name] << "\n";
+        else 
+        cout << "Sorry, no listing for " << name << "\n";
+    return 0;
+}
