290bb2068a828c833ad8da9f41488077c43cd0da
EvanLyu732 <zhengshushu1997@gmail.com>
2021-11-07 19:54:45 +0800

    add

---
+++ stl_regex.cpp
@@ -0,0 +1,25 @@
+#include <regex>
+#include <iostream>
+
+using std::regex;
+
+regex pat {R"(\w{2})"};
+
+// void test() 
+// {
+//     int lineno = 0;
+//     for (string line; getline(cin, line); ) {
+//         ++ lineno;
+//         smatch matches; 
+//         if (regex_search(line,matches,pat)
+//             std::cout << lineno << ": " << matches[0] << '\n';
+//     }
+// }
+
+int main()
+{
+    // regex_match();
+    // regex_search();
+    // regex_replace();
+
+}
