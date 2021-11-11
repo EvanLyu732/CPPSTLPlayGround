a6f567136812901f04c460f759a3740dbd2b9803
EvanLyu732 <zhengshushu1997@gmail.com>
2021-11-11 12:15:00 +0800

    add

---
+++ stl_filesystem.cpp
@@ -0,0 +1,19 @@
+#include <filesystem>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool checkFile(string file_name) 
+{
+
+
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        cerr << "Argument expected";
+        return 1;
+    }
+}
